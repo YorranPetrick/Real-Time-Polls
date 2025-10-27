75f8c6616e54fc411903b89dcc5122904fee872b
Yorran Petrick <yorranpetrick27@gmail.com>
2025-10-27 20:31:01 -0300

    feat(response_polls migration): Create Response Polls Migration using FlyWay

---
+++ src/main/resources/db/migration/V3__create_response_polls_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE response_polls (
+    id_response_poll UUID PRIMARY KEY,
+    answer VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    poll_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (poll_id) REFERENCES polls(id_poll) ON DELETE CASCADE
+);
